2012c29ffd7ac88b782062b9e11e6d8e8b5233d6
krishna <kv510341@gmail.com>
2024-05-11 12:52:48 +0530

    initialized mh app

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # Initialization:-
 
 1. create account in upload thing and copy and paste env variables.
+
+- bun add uploadthing @uploadthing/react
+- modify tailwind.config.ts file
+
 2. Go to shadcn website and init shacn {"bunx --bun shadcn-ui@latest add"}.
+
+3. Go to clerk and create an account.
+
+4. we need to install @clerk/themes (for light and dark mode)
+
+- bun add @clerk/nextjs
+
+# Landing Page
+
+1. https://bg.ibelick.com/ => this is the website where you can find modern bg snippets.
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -15,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider appearance={{ baseTheme: dark }}>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
